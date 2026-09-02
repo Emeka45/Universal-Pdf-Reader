9db052aae196c563c12648b99215dc62e179ccc7
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-02 17:13:16 +0100

    Create DocumentFormat.kt

---
+++ app/src/main/java/com/coeric/universalreader/DocumentFormat.kt
@@ -0,0 +1,21 @@
+package com.coeric.universalreader
+
+enum class DocumentFormat {
+    PDF,
+    EPUB,
+    MOBI,
+    AZW,
+    AZW3,
+    FB2,
+    CBZ,
+    CBR,
+    TXT,
+    HTML,
+    XHTML,
+    RTF,
+    DOC,
+    DOCX,
+    ODT,
+    MARKDOWN,
+    UNKNOWN
+}
