305d13d0dda084b17ac766db413ebfa7b87df477
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-02 21:09:36 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,19 +6,27 @@ plugins {
 
 android {
     namespace = "com.coeric.universalreader"
+
     compileSdk = 36
 
     defaultConfig {
         applicationId = "com.coeric.universalreader"
+
         minSdk = 26
+
         targetSdk = 36
+
         versionCode = 1
+
         versionName = "1.0"
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility =
+            JavaVersion.VERSION_17
+
+        targetCompatibility =
+            JavaVersion.VERSION_17
     }
 
     kotlinOptions {
@@ -31,16 +39,49 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.17.0")
-    implementation("androidx.activity:activity-compose:1.10.1")
 
-    implementation(platform("androidx.compose:compose-bom:2026.08.00"))
+    implementation(
+        "androidx.core:core-ktx:1.17.0"
+    )
 
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.foundation:foundation")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(
+        "androidx.activity:activity-compose:1.10.1"
+    )
 
-    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation(
+        platform(
+            "androidx.compose:compose-bom:2026.08.00"
+        )
+    )
+
+    implementation(
+        "androidx.compose.ui:ui"
+    )
+
+    implementation(
+        "androidx.compose.ui:ui-tooling-preview"
+    )
+
+    implementation(
+        "androidx.compose.foundation:foundation"
+    )
+
+    implementation(
+        "androidx.compose.material3:material3"
+    )
+
+    implementation(
+        "androidx.compose.material:material-icons-extended"
+    )
+
+    /*
+     * CBR / RAR support
+     */
+    implementation(
+        "com.github.junrar:junrar:8.1.0"
+    )
+
+    debugImplementation(
+        "androidx.compose.ui:ui-tooling"
+    )
 }
