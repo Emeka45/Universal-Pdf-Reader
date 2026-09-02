d43ad40c71079a64f27d15588de5b2eed56d6598
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-02 22:07:43 +0100

    Update DocumentFormat.kt

--- app/src/main/java/com/coeric/universalreader/DocumentFormat.kt
+++ app/src/main/java/com/coeric/universalreader/DocumentFormat.kt
@@ -1,21 +1,40 @@
 package com.coeric.universalreader
 
 enum class DocumentFormat {
+
     PDF,
+
     EPUB,
+
     MOBI,
+
     AZW,
+
     AZW3,
+
     FB2,
+
     CBZ,
+
     CBR,
+
+    ZIP,
+
     TXT,
+
     HTML,
+
     XHTML,
+
     RTF,
+
     DOC,
+
     DOCX,
+
     ODT,
+
     MARKDOWN,
+
     UNKNOWN
 }
