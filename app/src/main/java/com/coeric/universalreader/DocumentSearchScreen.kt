bed46050bc62ceef43cd51d9e7fc40cf05ca031a
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-03 10:01:02 +0100

    Update DocumentSearchScreen.kt

--- app/src/main/java/com/coeric/universalreader/DocumentSearchScreen.kt
+++ app/src/main/java/com/coeric/universalreader/DocumentSearchScreen.kt
@@ -14,10 +14,10 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Text
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -78,7 +78,7 @@ fun DocumentSearchScreen(
                 query.isNotBlank()
             ) {
 
-                DocumentSearch.searchEpub(
+                DocumentSearch.search(
                     document,
                     query
                 )
