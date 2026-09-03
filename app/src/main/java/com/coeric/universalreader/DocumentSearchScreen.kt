6d982ccf65f94a3ae7ee092c6111eff3b20f2932
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-03 14:56:50 +0100

    Update DocumentSearchScreen.kt

--- app/src/main/java/com/coeric/universalreader/DocumentSearchScreen.kt
+++ app/src/main/java/com/coeric/universalreader/DocumentSearchScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -31,6 +32,7 @@ import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DocumentSearchScreen(
     uri: android.net.Uri,
