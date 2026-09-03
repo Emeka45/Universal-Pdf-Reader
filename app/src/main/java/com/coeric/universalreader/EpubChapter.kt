ecd3e1e040670b7d28ca0f98fa5babc87072c436
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-03 09:32:51 +0100

    Update EpubChapter.kt

--- app/src/main/java/com/coeric/universalreader/EpubChapter.kt
+++ app/src/main/java/com/coeric/universalreader/EpubChapter.kt
@@ -5,10 +5,3 @@ data class EpubChapter(
     val content: String,
     val originalHtml: String = content
 )
-
-data class EpubDocument(
-    val title: String,
-    val author: String?,
-    val chapters: List<EpubChapter>,
-    val images: List<EpubImage> = emptyList()
-)
