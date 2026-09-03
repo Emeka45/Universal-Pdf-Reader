ce477233d519a59728dd62d914e73e2585142fc9
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-03 14:58:34 +0100

    Update MobiReaderScreen.kt

--- app/src/main/java/com/coeric/universalreader/MobiReaderScreen.kt
+++ app/src/main/java/com/coeric/universalreader/MobiReaderScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -18,6 +19,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.runtime.Composable
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MobiReaderScreen(
     uri: Uri,
