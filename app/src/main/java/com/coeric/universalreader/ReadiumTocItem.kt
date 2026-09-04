5f1b87167030f2ba92a00e10eb3985a1edd17eec
Chukwuemeka Ogbonna <165346188+Emeka45@users.noreply.github.com>
2026-09-04 01:16:43 +0100

    Update ReadiumTocItem.kt

--- app/src/main/java/com/coeric/universalreader/ReadiumTocItem.kt
+++ app/src/main/java/com/coeric/universalreader/ReadiumTocItem.kt
@@ -4,7 +4,7 @@ import org.readium.r2.shared.publication.Link
 
 data class ReadiumTocItem(
     val title: String,
-    val href: String,
+    val link: Link,
     val children: List<ReadiumTocItem> = emptyList()
 )
 
@@ -12,7 +12,7 @@ fun Link.toReadiumTocItem(): ReadiumTocItem {
 
     return ReadiumTocItem(
         title = title ?: "Untitled",
-        href = href.toString(),
+        link = this,
         children =
             children.map {
                 it.toReadiumTocItem()
